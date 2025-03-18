6c537a110d15d1eac18f54c0d548f1607143ca1d
Manisha Rana <manisharana02005@gmail.com>
2025-03-18 21:33:29 +0530

    Added logic for slider

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,24 +1,36 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card'
 import {FiChevronLeft, FiChevronRight} from 'react-icons/fi'
 
 
 function Testimonials(props) {
   let reviews=props.reviews;
+  const[index, setIndex]=useState(0);
 
   function leftShiftHandler(){
-
+    if(index-1 < 0){
+        setIndex(reviews.length-1);
+    }
+    else{
+        setIndex(index-1);
+    }
   }
   function rightShiftHandler(){
-
+    if(index+1 >= reviews.length){
+        setIndex(0);
+    } 
+    else{
+        setIndex(index+1);
+    }
   }
   function SurprizeHandler(){
-
+    let randomIndex=  Math.floor(Math.random()*reviews.length);
+    setIndex(randomIndex);
   }
   return (
     <div className=''>
-        <Card reviews={reviews[0]}/>
+        <Card reviews={reviews[index]}/>
 
         <div className=' flex text-3xl gap-3 mt-5 text-violet-400 font-bold mx-auto'>
         <button 
